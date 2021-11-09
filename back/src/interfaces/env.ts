a874b2c50fba115a2d2680744cea12d87b9e2f02
Raphael <paiva.raphaelt@gmail.com>
2021-11-09 19:34:09 +0100

    create interface for env variables

---
+++ back/src/interfaces/env.ts
@@ -0,0 +1,8 @@
+export interface EnvVariable extends NodeJS.ProcessEnv {
+	DATABASE_NAME?: string;
+	DATABASE_HOST?: string;
+	DATABASE_PORT?: string;
+	DATABASE_ROOT_USERNAME?: string;
+	DATABASE_ROOT_PASSWORD?: string;
+	PORT?: string;
+}
