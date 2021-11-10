098fda3d8dff5efec564a4350b85bccf42367d3c
Raphael <paiva.raphaelt@gmail.com>
2021-11-10 15:01:41 +0100

    add: express rate limit and express slow down

--- back/src/index.ts
+++ back/src/index.ts
@@ -3,6 +3,8 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
 import coookieParser from 'cookie-parser';
+import rateLimit from 'express-rate-limit';
+import slowDown from 'express-slow-down';
 
 import { checkSession } from '@/Middlewares/session';
 
@@ -14,6 +16,17 @@ import { routerProduct } from '@/Routes/Product';
 const PORT: number = +process.env.PORT! || 3000;
 const app: express.Application = express();
 
+const limiter: rateLimit.RateLimit = rateLimit({
+	windowMs: 60000,
+	max: 300,
+});
+
+const slower = slowDown({
+	windowMs: 60000,
+	delayAfter: 200,
+	delayMs: 1000,
+});
+
 connectAtMongoDB();
 app.use(morgan('dev'));
 app.disable('x-powered-by');
@@ -22,6 +35,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(coookieParser());
+app.use(limiter);
+app.use(slower);
 
 app.use(`${process.env.URL_API}/account`, routerAccount);
 app.use(`${process.env.URL_API}/products`, checkSession, routerProduct);
