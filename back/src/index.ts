daea3f437075b69729555fb9133d0e24f8f33ecd
Raphael <paiva.raphaelt@gmail.com>
2021-11-10 03:49:01 +0100

    add: routerAccount

--- back/src/index.ts
+++ back/src/index.ts
@@ -4,16 +4,19 @@ import cors from 'cors';
 
 import { connectAtMongoDB } from '@/Config/config';
 
+import { routerAccount } from '@/Routes/Account';
+
 const PORT: number = +process.env.PORT! || 3000;
 const app: express.Application = express();
 
 connectAtMongoDB();
 
+app.disable('x-powered-by');
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => res.json('hello world'));
+app.use(`${process.env.URL_API}/account`, routerAccount);
 
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
