6760650fb168461d05a3335895a8247b3c2e2fc4
Raphael <paiva.raphaelt@gmail.com>
2021-11-10 10:20:00 +0100

    add: create new middleware which check if session exist and check jwttoken

---
+++ back/src/middlewares/session.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from 'express';
+
+import { verifyToken } from '@/Utils/jwt';
+import { errorResponse } from '@/Utils/responses';
+
+type Cookie = { token: string };
+
+const checkSession = async (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
+	try {
+		const cookie: Cookie = req.cookies.xAccessToken;
+		const secret: string = process.env.SECRET_TOKEN!;
+
+		if (!cookie) {
+			res.json(errorResponse(404, "doesn't exist"));
+		}
+
+		const checkToken = await verifyToken(cookie.token, secret);
+		req.decoded = checkToken;
+		next();
+	} catch (err: any) {
+		res.json(errorResponse(404, err.message));
+	}
+};
+
+export { checkSession };
