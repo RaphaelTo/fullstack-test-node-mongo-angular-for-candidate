5126134634dad649e95a1d9dad0dc241194316ba
Raphael <paiva.raphaelt@gmail.com>
2021-11-10 12:56:06 +0100

    add: method deleteProduct and checkProductExist

--- back/src/controllers/Product.ts
+++ back/src/controllers/Product.ts
@@ -23,4 +23,40 @@ const addProduct = async (
 	}
 };
 
-export { addProduct };
+const deleteProduct = async (idProduct: string, idAccount: string) => {
+	try {
+		const checkProduct = await checkProductExist({ idProduct, idAccount });
+
+		if (!checkProduct) {
+			throw new Error("Product doesn't exist");
+		}
+
+		await Product.deleteOne({ idProduct, idAccount });
+
+		return 'Product has been deleted';
+	} catch (err) {
+		throw err;
+	}
+};
+
+const checkProductExist = async (params: {
+	idProduct: string;
+	idAccount: string;
+}) => {
+	try {
+		const findProduct = await Product.findOne({
+			_id: params.idProduct,
+			idAccount: params.idAccount,
+		}).exec();
+
+		if (!findProduct) {
+			return false;
+		}
+
+		return findProduct;
+	} catch (err) {
+		throw err;
+	}
+};
+
+export { addProduct, deleteProduct };
