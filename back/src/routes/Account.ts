fbaa772089d851aa8524542d688454d69b618e49
Raphael <paiva.raphaelt@gmail.com>
2021-11-10 04:59:16 +0100

    add: route signin

--- back/src/routes/Account.ts
+++ back/src/routes/Account.ts
@@ -1,6 +1,6 @@
 import express, { Router } from 'express';
 
-import { signupAction } from '@/Controllers/Account';
+import { signupAction, signinAction } from '@/Controllers/Account';
 
 import { objectValidator } from '@/Types/middlewares/validator';
 import { AccountValidator } from '@/Interfaces/validators/Account';
@@ -16,18 +16,32 @@ const validatorSchema: objectValidator<AccountValidator> = {
 	schema: signupValidator,
 };
 
-routerAccount.post(
-	'/signup',
-	(req, res, next) =>
-		validatorParamsBodyQueries(req, res, next)([validatorSchema]),
-	async (req, res) => {
-		try {
-			const addAccount = await signupAction(req.body);
-			res.json(successResponse(201, addAccount));
-		} catch (err: any) {
-			return res.json(errorResponse(404, err.message));
-		}
-	},
-);
+routerAccount
+	.post(
+		'/signup',
+		(req, res, next) =>
+			validatorParamsBodyQueries(req, res, next)([validatorSchema]),
+		async (req, res) => {
+			try {
+				const addAccount = await signupAction(req.body);
+				res.json(successResponse(201, addAccount));
+			} catch (err: any) {
+				return res.json(errorResponse(404, err.message));
+			}
+		},
+	)
+	.post(
+		'/signin',
+		(req, res, next) =>
+			validatorParamsBodyQueries(req, res, next)([validatorSchema]),
+		async (req, res) => {
+			try {
+				const login = await signinAction(req.body);
+				res.json(successResponse(201, login));
+			} catch (err: any) {
+				return res.json(errorResponse(404, err.message));
+			}
+		},
+	);
 
 export { routerAccount };
