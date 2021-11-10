de2c3982a181d8a69a2d99f365d039a58e8f2f33
Raphael <paiva.raphaelt@gmail.com>
2021-11-10 12:55:31 +0100

    add: delete Product, and refacto catch post product

--- back/src/routes/Product.ts
+++ back/src/routes/Product.ts
@@ -1,9 +1,15 @@
 import express, { Router } from 'express';
 
-import { addProduct } from '@/Controllers/Product';
+import { addProduct, deleteProduct } from '@/Controllers/Product';
 
-import { addProductValidator } from '@/Validators/Product';
-import { ProductValidator } from '@/Interfaces/validators/Product';
+import {
+	addProductValidator,
+	deleteProductValidator,
+} from '@/Validators/Product';
+import {
+	ProductValidator,
+	DeleteProductValidator,
+} from '@/Interfaces/validators/Product';
 import { objectValidator } from '@/Types/middlewares/validator';
 
 import { validatorParamsBodyQueries } from '@/Middlewares/validator';
@@ -18,18 +24,42 @@ const validatorAddProductSchema: objectValidator<ProductValidator> = {
 	schema: addProductValidator,
 };
 
-routerProduct.post(
-	'/product',
-	checkSession,
-	validatorParamsBodyQueries([validatorAddProductSchema]),
-	async (req, res) => {
-		try {
-			const add = addProduct(req.body, req.decoded.id);
-			res.json('add Product');
-		} catch (err) {
-			res.json(errorResponse(404, err.message));
-		}
-	},
-);
+const validatorDeleteProductSchema: objectValidator<DeleteProductValidator> = {
+	type: 'params',
+	schema: deleteProductValidator,
+};
+
+routerProduct
+	.post(
+		'/product',
+		checkSession,
+		validatorParamsBodyQueries([validatorAddProductSchema]),
+		async (req, res) => {
+			try {
+				const add = await addProduct(req.body, req.decoded.id);
+
+				res.status(201).json(successResponse(201, add));
+			} catch (err: any) {
+				res.status(404).json(errorResponse(404, err.message));
+			}
+		},
+	)
+	.delete(
+		'/product/:idProduct',
+		checkSession,
+		validatorParamsBodyQueries([validatorDeleteProductSchema]),
+		async (req, res) => {
+			try {
+				const dlProduct = await deleteProduct(
+					req.params.idProduct,
+					req.decoded.id,
+				);
+
+				res.status(200).json(successResponse(200, dlProduct));
+			} catch (err: any) {
+				res.status(404).json(errorResponse(404, err.message));
+			}
+		},
+	);
 
 export { routerProduct };
