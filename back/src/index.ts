9db2561969472fecfe11eb08d02e45aeb031ab9a
Raphael <paiva.raphaelt@gmail.com>
2021-11-10 10:19:06 +0100

    add: two middleware morgran and cookieParser

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import helmet from 'helmet';
+import morgan from 'morgan';
 import cors from 'cors';
+import coookieParser from 'cookie-parser';
 
 import { connectAtMongoDB } from '@/Config/config';
 
@@ -10,12 +12,13 @@ const PORT: number = +process.env.PORT! || 3000;
 const app: express.Application = express();
 
 connectAtMongoDB();
-
+app.use(morgan('dev'));
 app.disable('x-powered-by');
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(coookieParser());
 
 app.use(`${process.env.URL_API}/account`, routerAccount);
 
