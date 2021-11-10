62bf3e0d708495f79d5c326c694a04b93ff565c0
Raphael <paiva.raphaelt@gmail.com>
2021-11-10 10:18:23 +0100

    refacto & add: expiresIn added, verifyToken method created

--- back/src/utils/jwt.ts
+++ back/src/utils/jwt.ts
@@ -1,13 +1,28 @@
 import jwt from 'jsonwebtoken';
+import util from 'util';
 
 const createToken = async (payload: object): Promise<string> => {
 	try {
 		const secret: string = process.env.SECRET_TOKEN!;
-		const createToken = await jwt.sign(payload, secret, { algorithm: 'HS256' });
+		const createToken: string = await jwt.sign(payload, secret, {
+			algorithm: 'HS256',
+			expiresIn: '10h',
+		});
 		return createToken;
 	} catch (err) {
 		throw err;
 	}
 };
 
-export { createToken };
+const verifyToken = async (token: string, secret: string): Promise<object> => {
+	try {
+		const promisifyVerify = util.promisify<string, string>(jwt.verify);
+
+		const checkToken = await promisifyVerify(token, secret);
+		return checkToken;
+	} catch (err) {
+		throw err;
+	}
+};
+
+export { createToken, verifyToken };
